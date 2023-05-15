6c08cf4dd2de49212e8be59c0755480dc3e10309
Reubencfernandes <18reuchagasfernandes@gmail.com>
2023-05-15 17:48:39 +0530

    some changed

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 # using the facebook/bart-large-cnn trained model to summarize text
-'''Here is an Example
-"Never Gonna Give You Up" is a classic 1980s pop song with a catchy melody and upbeat tempo. It was written and produced by Stock Aitken Waterman, a British songwriting and production team who were responsible for many hits during that era.
-The song's music video features Rick Astley performing the song and dancing in a variety of locations, including a studio and a city street. Astley's distinctive baritone voice, combined with the song's memorable hook and upbeat rhythm, helped to make it a huge success both in the UK and internationally.
-In the years since its release, "Never Gonna Give You Up" has become a cultural touchstone, thanks in large part to the internet phenomenon of rickrolling. This involves sending a link to someone with the promise of leading them to one thing, but instead redirecting them to the music video for the song. The practice of rickrolling gained popularity in the mid-2000s and has been used as a playful prank, a form of internet trolling, and even a political statement. Despite being associated with the prank, the song itself remains a beloved classic of 80s pop music.
-'''
 from transformers import pipeline
-import tkinter
+from tkinter import *
 def makeSmall():
+    ARTICLE = inputtext.get("1.0", 'end-1c')
+    firstwords =ARTICLE.split(" ")
+    lengthoffirstwords = len(firstwords)
+    getfirst.config(text="Length of words is %d"%lengthoffirstwords)
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    ARTICLE = inputtext.get("1.0",'end-1c')
     converted = summarizer(ARTICLE,max_length=142, min_length=100, do_sample=True)
     outputtext.configure(text=converted[0]['summary_text'],font="Arial",aspect=500)
-root = tkinter.Tk()
+    last = converted[0]['summary_text'].split(" ")
+    lengthoflast = len(last)
+    lastwords.config(text="Length of summarized text is %d"%lengthoflast)
+root = Tk()
 root.geometry("1000x1000")
 root.title("Text Summarization")
-inputtext = tkinter.Text(root, height=20,width=100)
-outputtext = tkinter.Message(root,cursor='hand2')
-button = tkinter.Button(root, text="Summarize", bg="#bf1d1d", fg="white", font=("Arial", 20), bd=0, padx=10, pady=10, activebackground="#1da1bf",command=lambda:makeSmall())
+getfirst = Label(root)
+lastwords = Label(root)
+inputtext = Text(root, height=20,width=100)
+outputtext = Message(root,cursor='hand2')
+button = Button(root, text="Summarize", bg="#bf1d1d", fg="white", font=("Arial", 20), bd=0, padx=10, pady=10, activebackground="#1da1bf",command=makeSmall)
+getfirst.pack()
 inputtext.pack()
 button.pack(padx=20,pady=20)
 outputtext.pack()
+lastwords.pack()
 root.mainloop()
-
-
-
- 
